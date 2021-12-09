580001760e46a8246ec79bdb9cc3deab13407d42
iohcidnal <landicho.ian@gmail.com>
2021-12-08 23:20:56 -0700

    Initialize project

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let counter = 0;
+</script>
+
+<main>
+  <h1>Welcome to SvelteKit</h1>
+  <h2>Current count is {counter}</h2>
+  <input type="button" value="+1" on:click={() => counter++} />
+</main>
