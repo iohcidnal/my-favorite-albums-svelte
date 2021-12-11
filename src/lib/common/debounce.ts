7eca71458832a552e863318f5d6caab6e739f46f
iohcidnal <landicho.ian@gmail.com>
2021-12-11 12:13:42 -0700

    Debounce search term

---
+++ src/lib/common/debounce.ts
@@ -0,0 +1,11 @@
+import { onDestroy } from 'svelte';
+
+export default function debounce(fn: () => any, delay?: number) {
+  let timeout: NodeJS.Timeout;
+
+  onDestroy(() => {
+    clearTimeout(timeout);
+  });
+
+  return new Promise(resolve => (timeout = setTimeout(() => resolve(fn()), delay ?? 800)));
+}
---
+++ src/routes/top-albums/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { debounce } from '$lib/common';
+
+  let searchTerm: string;
+  let debouncedSearchTerm: string;
+
+  $: {
+    debounce(() => searchTerm).then((value: string) => (debouncedSearchTerm = value));
+  }
+
+  $: {
+    console.log('debouncedSearchTerm :>> ', debouncedSearchTerm);
+  }
+</script>
+
+<main class="px-10 py-6">
+  <div class="form-control">
+    <input
+      class="input input-bordered input-info"
+      type="text"
+      placeholder="Search albums"
+      bind:value={searchTerm}
+    />
+  </div>
+</main>
