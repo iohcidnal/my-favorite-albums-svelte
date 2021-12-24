7d9fb0ad0a1639f5f78d254027325e66b3d1c811
iohcidnal <landicho.ian@gmail.com>
2021-12-23 23:58:22 -0700

    Make searh results component reusable

--- src/lib/components/search-result.svelte
+++ src/lib/components/search-result.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-  import { albums } from '$lib/common';
   import AlbumCard from './album-card.svelte';
+  import type { AlbumCardProps } from '$lib/common';
+
+  export let albums: AlbumCardProps[];
 </script>
 
 <div class="flex flex-col flex-1 md:flex-wrap md:flex-row justify-center gap-4 pt-4 pb-24">
-  {#each $albums as album (album.id)}
+  {#each albums as album (album.id)}
     <AlbumCard {album} />
   {/each}
 </div>
